2247a4cb38d2aca949cfc13a9f3a334288313edd
Danizhj <danikhihanik@gmail.com>
2026-04-04 01:29:19 +0500

    Create app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Chat from './components/Chat';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
